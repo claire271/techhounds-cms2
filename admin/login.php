d2979cf3519425a762566cf4112b799718e4b9f2
Tiger Huang <tigerrun1998@gmail.com>
2015-04-14 19:05:47 -0400

    Added form on login page. Added blank file for session stuff.

---
+++ admin/login.php
@@ -0,0 +1,16 @@
+<?php
+$action = isset( $_GET['action'] ) ? $_GET['action'] : "";
+?>
+<html>
+	<head>
+		<title>Admin Login</title>
+	</head>
+	<body>
+		<h1>Admin Login</h1>
+		<form action="index.php" method="POST">
+			Username: <input type="text" name="username"><br>
+			Password: <input type="password" name="password"><br>
+			<input type="submit" value="Login">
+		</form>
+	</body>
+</html>
