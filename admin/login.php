8b0118265b10ec360586d28227d5f31beface790
Vincent <tqm397@gmail.com>
2015-04-14 20:19:11 -0400

    Began some basic css to make the login page a little nicer.

--- admin/login.php
+++ admin/login.php
@@ -4,6 +4,7 @@ $action = isset( $_GET['action'] ) ? $_GET['action'] : "";
 <html>
 	<head>
 		<title>Admin Login</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
 	</head>
 	<body>
 		<h1>Admin Login</h1>
