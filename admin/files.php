06a3fe3ff45a569aa53a6dc71b41c19af4c599e9
Tiger Huang <tigerrun1998@gmail.com>
2015-04-15 15:48:57 -0400

    Added ability to navigate directories.

--- admin/files.php
+++ admin/files.php
@@ -17,7 +17,7 @@ for($i = 0;$i < count($files);$i++) {
 	$name = $files[$i];
 	$files[$i] = new stdClass();
 	$files[$i]->name = $name;
-	if(is_dir($file_path . $name)) {
+	if(is_dir($file_path . "/" . $name)) {
 		$files[$i]->flag = "dir";
 	}
 	else {
@@ -26,11 +26,23 @@ for($i = 0;$i < count($files);$i++) {
 }
 
 foreach($pages as $page) {
+	$match = false;
+	foreach($files as $file) {
+		if(basename($page->out_path) == $file->name) {
+			$match = true;
+			$file->flag = "dynamic";
+			$file->index = $page->index;
+		}
+	}
+	if(!$match && dirname($page->out_path) == $path) {
+		$file = new stdClass();
+		$file->name = basename($page->out_path);
+		$file->flag = "dynamic";
+		$file->index = $page->index;
+		array_push($files,$file);
+	}
 	print_r($page);
 }
-
-
-
 
 echo "<pre>";
 print_r($files);
@@ -45,8 +57,13 @@ echo "</pre>";
 		<h1>Index of <?php echo $path ?></h1>
 		<?php
 		foreach($files as $file) {
-			echo $file->flag;
-			echo $file->name;
+			if($file->flag == "dir") {
+		?>
+			<a href="files.php?path=<?php echo (($path == "/") ? $path : ($path . "/")) . $file->name ?>" style="color:#0000FF"><?php echo $file->name ?></a><br/>
+		<?php
+		  //echo $file->flag . ":";
+		  //echo $file->name . "\n";
+		  }
 		}
 		?>
 	</body>
