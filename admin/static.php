3bfb908f85cdaa2e4a9debf283463eddb275c3d9
Tiger Huang <tigerrun1998@gmail.com>
2015-04-16 15:45:15 -0400

    Finished the static editor.

---
+++ admin/static.php
@@ -0,0 +1,30 @@
+<?php
+require("util.php");
+
+define("ROOT_PATH",cleanPath($_SERVER['DOCUMENT_ROOT']));
+
+$path = cleanPath($_GET['path']);
+$file_path = ROOT_PATH . $path;
+
+$contents = file_get_contents($file_path);
+
+if($action == "save") {
+	file_put_contents($file_path,$_POST["contents"]);
+	header( "Location: static.php?path=" . $path);
+}
+
+?>
+<html>
+	<head>
+		<title>Static Editor</title>
+	</head>
+	<body>
+		<h1><?php echo basename($path) ?></h1>
+		<form action="static.php?action=save&path=<?php echo $path ?>" method="POST">
+			<textarea name="contents" placeholder="The contents of the file" style="height: 30em; width: 80%;"><?php echo $contents ?></textarea><br>
+			<input type="submit" value="Save">
+			<a href="static.php?path=<?php echo $path ?>">Cancel</a>
+			<a href="files.php?path=<?php echo dirname($path) ?>">Back</a>
+		</form>
+	</body>
+</html>
