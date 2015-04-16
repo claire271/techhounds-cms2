d5364ee49e681ffda2d9d6987ccf7a115814cb6c
Tiger Huang <tigerrun1998@gmail.com>
2015-04-15 22:14:36 -0400

    Added filename fixing on all pages. Added links to (nonexistent) editors on the file system viewer.

--- admin/files.php
+++ admin/files.php
@@ -1,7 +1,7 @@
 <?php
 require("util.php");
 
-define("ROOT_PATH",$_SERVER['DOCUMENT_ROOT'] . "/");
+define("ROOT_PATH",cleanPath($_SERVER['DOCUMENT_ROOT']));
 
 $pages_table = Table::open("cms2-pages");
 if(!$pages_table) {
@@ -10,6 +10,7 @@ if(!$pages_table) {
 $pages = $pages_table->getRows();
 
 $path = isset( $_GET['path'] ) ? $_GET['path'] : "/";
+$path = cleanPath($path);
 $file_path = ROOT_PATH . $path;
 $files = scandir($file_path);
 
@@ -34,19 +35,14 @@ foreach($pages as $page) {
 			$file->index = $page->index;
 		}
 	}
-	if(!$match && dirname($page->out_path) == $path) {
+	if(!$match && cleanPath(dirname($page->out_path)) == cleanPath($path)) {
 		$file = new stdClass();
 		$file->name = basename($page->out_path);
 		$file->flag = "dynamic";
 		$file->index = $page->index;
 		array_push($files,$file);
 	}
-	print_r($page);
 }
-
-echo "<pre>";
-print_r($files);
-echo "</pre>";
 
 ?>
 <html>
@@ -57,12 +53,21 @@ echo "</pre>";
 		<h1>Index of <?php echo $path ?></h1>
 		<?php
 		foreach($files as $file) {
+			$new_path = cleanPath($path . "/" . $file->name);
 			if($file->flag == "dir") {
 		?>
-			<a href="files.php?path=<?php echo (($path == "/") ? $path : ($path . "/")) . $file->name ?>" style="color:#0000FF"><?php echo $file->name ?></a><br/>
+			<a href="files.php?path=<?php echo $new_path ?>" style="color:#0000FF"><?php echo $file->name ?></a><br/>
 		<?php
-		  //echo $file->flag . ":";
-		  //echo $file->name . "\n";
+		  }
+		  else if($file->flag == "static") {
+		?>
+			<a href="static.php?path=<?php echo $new_path ?>" style="color:#FF0000"><?php echo $file->name ?></a><br/>
+		<?php
+		  }
+		  else if($file->flag == "dynamic") {
+		?>
+			<a href="dynamic.php?path=<?php echo $new_path ?>&index=<?php echo $file->index ?>" style="color:#FF00FF"><?php echo $file->name ?></a><br/>
+		<?php
 		  }
 		}
 		?>
--- admin/util.php
+++ admin/util.php
@@ -1,23 +1,4 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . "/db/db.php");
-
-define("ADMIN_ROOT",$_SERVER['DOCUMENT_ROOT'] . "/admin/");
-session_save_path(ADMIN_ROOT . "session/");
-session_start();
-
-//Only for login/logout page
-if(defined("LOGOUT")) {
-	unset( $_SESSION['username'] );
-}
-//See if logged on already and not on main page
-else if(!defined("MAIN") && !isset($_SESSION["username"])) {
-	header( "Location: login.php?action=fail" );
-}
-
-$action = isset( $_GET['action'] ) ? $_GET['action'] : "";
-
-//Some utility functions
-
 //Always returns with no trailing slash
 function cleanPath($path) {
 	$path = str_replace("\\","/",$path);
@@ -39,5 +20,22 @@ function cleanPath($path) {
 
   return ($leading ? "/" : "") . implode("/",$parts);
 }
+
+require(cleanPath($_SERVER['DOCUMENT_ROOT'] . "/db/db.php"));
+
+session_save_path(cleanPath($_SERVER['DOCUMENT_ROOT'] . "/admin/session"));
+session_start();
+
+//Only for login/logout page
+if(defined("LOGOUT")) {
+	unset( $_SESSION['username'] );
+}
+//See if logged on already and not on main page
+else if(!defined("MAIN") && !isset($_SESSION["username"])) {
+	header( "Location: login.php?action=fail" );
+}
+
+$action = isset( $_GET['action'] ) ? $_GET['action'] : "";
+
 ?>
 
