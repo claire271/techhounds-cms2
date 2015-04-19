1e90c4e7010ba4470c6ffa8c0743c8f46fafe580
Tiger Huang <tigerrun1998@gmail.com>
2015-04-19 17:38:42 -0400

    Started on upload file page.

---
+++ admin/upload.php
@@ -0,0 +1,16 @@
+<?php
+require("util.php");
+
+
+?>
+<html>
+	<head>
+		<title>Upload Files</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div class="body-container">
+			<h1>File Upload</h1>
+		</div>
+	</body>
+</html>
