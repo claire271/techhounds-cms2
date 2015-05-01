4810c5627711e8253470685b33f2c3ba3edcaa90
Tiger Huang <tigerrun1998@gmail.com>
2015-05-01 15:43:09 -0400

    Started integrating ace editor.

--- admin/static.php
+++ admin/static.php
@@ -28,4 +28,38 @@ if($action == "save") {
 			</form>
 		</div>
 	</body>
+	<script src="/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+	//Ace stuff
+	$(function () {
+		$('textarea[data-editor]').each(function () {
+			var textarea = $(this);
+			
+			var mode = textarea.data('editor');
+			
+			var editDiv = $('<div>', {
+				position: 'absolute',
+				width: textarea.width(),
+				height: textarea.height(),
+				'class': textarea.attr('class')
+			}).insertBefore(textarea);
+			
+			//textarea.css('visibility', 'hidden');
+			textarea.css('display', 'none');
+			
+			var editor = ace.edit(editDiv[0]);
+			//editor.renderer.setShowGutter(false);
+			editor.getSession().setValue(textarea.val());
+			editor.getSession().setMode("ace/mode/" + mode);
+			editor.setKeyboardHandler("ace/keyboard/ace");
+			editor.setTheme("ace/theme/idle_fingers");
+			
+			test = editor;
+			// copy back to textarea on form submit
+			textarea.closest('form').submit(function () {
+				textarea.val(editor.getSession().getValue());
+			})
+		});
+	});
+	</script>
 </html>
